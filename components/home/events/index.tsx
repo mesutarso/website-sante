d34b89362d14341a761dfdd132bfdd7af206e370
mechack tambwe <mesutarso@users.noreply.github.com>
2025-03-29 19:27:25 +0100

    fix issues

--- components/home/events/index.tsx
+++ components/home/events/index.tsx
@@ -8,25 +8,25 @@ import { ArrowRight } from "lucide-react";
 
 function EventsSection() {
   return (
-    <div className={"relative min-h-[500px] bg-yellowSky"}>
-      <Section>
-        <Container className="flex flex-col  justify-center  min-h-[600px]">
-          <h1 className={"heading mb-10  text-blue text-center"}>
+    <div className={"relative  bg-yellowSky"}>
+      <Section className="py-10">
+        <Container className="flex flex-col  justify-center  ">
+          <h1 className={"heading mb-8  text-blue text-center"}>
             Événements
           </h1>
 
-          <div className="space-y-4 p-2 mx-auto">
+          <div className="space-y-4 p-2 mx-auto grid grid-cols-1 md:grid-cols-2  gap-4">
             {events.map((event, index) => (
               <EventCard key={index} {...event} />
             ))}
           </div>
 
-          <div className="text-center mt-10">
+          {/* <div className="text-center mt-10">
             <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-md font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 hover:transparent dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-white h-10 px-4 py-6 bg-blue text-white xl:txt-btn-big border-4 border-blue">
               Voir les évenements
               <ArrowRight />
             </button>
-          </div>
+          </div> */}
         </Container>
       </Section>
     </div>
