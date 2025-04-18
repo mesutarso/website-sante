6c4aa818a139b507b26ed2f4435e57243645761c
mechack tambwe <mesutarso@users.noreply.github.com>
2025-04-18 09:36:21 +0100

    fix issues

--- app/direction-generale/page.tsx
+++ app/direction-generale/page.tsx
@@ -9,7 +9,7 @@ interface CooperationCategory {
 }
 
 const categories: CooperationCategory[] = [
-  { title: "Direction Generale de Lutte contre la Maladie", desc: "La DGLM conçoit et met en œuvre des politiques, des stratégies et des normes pour surveiller les épidémies, les laboratoires et l'hygiène publique. Elle coordonne les programmes de lutte contre les maladies et promeut les pratiques innovantes pour améliorer la qualité de vie.",color: "bg-blueSky" },
+  { title: "Direction Generale de Lutte contre la Maladie", desc: "La DGLM conçoit et met en œuvre des politiques, des stratégies et des normes pour surveiller les épidémies, les laboratoires et l'hygiène publique. Elle coordonne les programmes de lutte contre les maladies et promeut les pratiques innovantes pour améliorer la qualité de vie.", color: "bg-blueSky" },
   { title: "Direction Generale de l organisation et de Gestion des Services et Soins de Sante", desc: "La DGOGSS conçoit et met en œuvre des politiques et des stratégies pour l'organisation et la gestion des soins de santé, tout en gérant le système national d'information sanitaire. Elle soutient également les projets d'établissement et suit la mise en œuvre des politiques relatives aux soins de santé.", color: "bg-green" },
   { title: "Direction Financiere et Administrative", desc: "Elle assure l'engagement comptable, la liquidation et l'ordonnancement des dépenses du budget du Ministère de la Santé. Elle élabore en collaboration avec les services internes les prévisions, tient et gère l'inventaire des biens meubles et immeubles et appuie les services du Minist", color: "bg-blue" },
   { title: "Direction des ressources humaines", desc: "Conformément à la stratégie de gestion intégrée de gestion des ressources humaines telles qu'édictées par le Ministère de la Fonction Publique, la Direction des Ressources Humaines assure le développement des compétences des ressources humaines du Secrétariat général à la Santé. Elle contribue à l'élaboration des politiques et des stratégies, veille à l'exécution des textes règlementaires et législatifs relatifs à la gestion des ressources humaines de l'Etat. Afin de promouvoir le dialogue et la communication interpersonnelle au sein du Secrétariat général à la Santé, elle promeut des activités culturelles, sportives et ludiques et met en place des conditions optimales de travail pour les agents. Elle assure, en collaboration avec les services compétents du Ministère de la Fonction Publique, la mise en oeuvre du cadre et des structures organiques ainsi que du référentiel des emplois. ", color: "bg-blueMin" },
@@ -29,27 +29,27 @@ export default function page() {
             <p className="mt-3 max-w-xl mx-auto">Ci-dessous la liste des directions centrales du Ministère de la Santé.</p>
           </div>
 
-          <div className="grid grid-cols-3 gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
             {categories.map((category, index) => (
-              <Link  key={index} href={``}>
-              <div
-               
-                className={`${category.color} p-6 rounded-3xl flex flex-col justify-between min-h-[350px]`}
-              >
-                <CircleArrowRight
-                  className="text-white w-10 h-10 "
-                  aria-hidden="true"
-                  strokeWidth={0.5}
-                />
-                <h2 className="text-white text-xl font-bold mt-auto">
-                  <span className="block font-rocgrotesk uppercase text-xl mb-2">
-                    {category.title}
-                  </span>
-                </h2>
-                <p className="text-white line-clamp-4 text-sm">
-                  {category.desc}
-                </p>
-              </div>
+              <Link key={index} href={``}>
+                <div
+
+                  className={`${category.color} p-6 rounded-3xl flex flex-col justify-between min-h-[350px]`}
+                >
+                  <CircleArrowRight
+                    className="text-white w-10 h-10 "
+                    aria-hidden="true"
+                    strokeWidth={0.5}
+                  />
+                  <h2 className="text-white text-xl font-bold mt-auto">
+                    <span className="block font-rocgrotesk uppercase text-xl mb-2">
+                      {category.title}
+                    </span>
+                  </h2>
+                  <p className="text-white line-clamp-4 text-sm">
+                    {category.desc}
+                  </p>
+                </div>
               </Link>
             ))}
           </div>
--- components/cooperation/bilateral.tsx
+++ components/cooperation/bilateral.tsx
@@ -9,7 +9,7 @@ interface CooperationCategory {
 }
 
 const categories: CooperationCategory[] = [
-  { title: "Le Cabinet du Ministre", desc: "Le Ministre et les membres du Cabinet",color: "bg-blueSky" },
+  { title: "Le Cabinet du Ministre", desc: "Le Ministre et les membres du Cabinet", color: "bg-blueSky" },
   { title: "Le Secretariat General", desc: "Le membres du Secrétariat Général du Ministère", color: "bg-green" },
   { title: "Inspection Generale", desc: "L'inspection générale du Ministère", color: "bg-blue" },
   { title: "Directions Generales", desc: "Les directions centrales du Ministère", color: "bg-blueMin" },
@@ -22,32 +22,32 @@ export default function BilateralGrid() {
         <Container className={""}>
           <div className="text-center mb-40 ">
             <h1 className="text-5xl font-black text-[#1a2f4b]">
-            Organisation
+              Organisation
             </h1>
             <p className="mt-3 max-w-xl mx-auto">Le Ministère de la Santé Publique, Hygiène et Prévention de la République Démocratique du Congo est constitué de quatre principales entités.</p>
           </div>
 
-          <div className="grid grid-cols-4 gap-12">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
             {categories.map((category, index) => (
-              <Link  key={index} href={``}>
-              <div
-               
-                className={`${category.color} p-6 rounded-3xl flex flex-col justify-between min-h-[350px]`}
-              >
-                <CircleArrowRight
-                  className="text-white w-10 h-10 "
-                  aria-hidden="true"
-                  strokeWidth={0.5}
-                />
-                <h2 className="text-white text-xl font-bold mt-auto">
-                  <span className="block font-rocgrotesk uppercase text-3xl">
-                    {category.title}
-                  </span>
-                </h2>
-                <p className="text-white">
-                  {category.desc}
-                </p>
-              </div>
+              <Link key={index} href={``}>
+                <div
+
+                  className={`${category.color} p-6 rounded-3xl flex flex-col justify-between min-h-[350px]`}
+                >
+                  <CircleArrowRight
+                    className="text-white w-10 h-10 "
+                    aria-hidden="true"
+                    strokeWidth={0.5}
+                  />
+                  <h2 className="text-white text-xl font-bold mt-auto">
+                    <span className="block font-rocgrotesk uppercase text-3xl">
+                      {category.title}
+                    </span>
+                  </h2>
+                  <p className="text-white">
+                    {category.desc}
+                  </p>
+                </div>
               </Link>
             ))}
           </div>
