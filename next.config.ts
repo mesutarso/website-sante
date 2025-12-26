66aa71c418614c81c5f293c175056df232756cb0
mechack tambwe <mesutarso@users.noreply.github.com>
2025-12-26 05:01:15 +0100

    fix issues

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    viewTransition: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
